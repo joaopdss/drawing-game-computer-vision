bd279c6bd304c6e400348bcf988ce2f1a169a8f9
João Pedro dos Santos <contactjoaopedrods@gmail.com>
2022-05-30 22:26:08 -0300

    att cam_painter and model

--- cam_painter.py
+++ cam_painter.py
@@ -1,13 +1,27 @@
 import numpy as np
 import cv2
-import os
+import tensorflow as tf
 import hand_tracking_module as hand_tracking
+import time
 
+def process_image(img, shape):
+    img = cv2.resize(img, shape)
+    img = img / 255
+    return img
+
+model = tf.keras.models.load_model("draw_model.h5")
+
+labels = ['circle', 'sun', 'cloud', 'pants', 'rainbow', 'banana']
+actual_label = ""
+nn_prediction = ""
+draw_finished = False
 header_img = cv2.imread("Header/header.jpg")
 
+# Get capture and set config
 cap = cv2.VideoCapture(0)
 cap.set(3, 1288)
 cap.set(4, 728)
+
 hand_detector = hand_tracking.handDetector()
 draw_color = (0, 0, 255)
 
@@ -15,11 +29,23 @@ selection_mode = [0, 1]
 idx_selection = 0
 xp, yp = 0, 0
 img_canvas = np.zeros((720, 1280, 3), np.uint8)
-
+first_frame = True
+start_time = time.time()
+end_time = 0
 while True:
     _, frame = cap.read()
 
     if _:
+        if draw_finished or first_frame:
+            idx_label = np.random.randint(0, 6)
+            print(idx_label)
+            actual_label = labels[idx_label]
+
+            if first_frame:
+                first_frame = False
+            if draw_finished:
+                draw_finished = False
+
         frame = cv2.flip(frame, 1)
         frame = hand_detector.find_hands(frame)
         lm_list = hand_detector.find_position(frame)
@@ -32,28 +58,31 @@ while True:
             fingers = hand_detector.fingers_up()
 
             if fingers[1] and fingers[2]:
-                print("Pen & rubber selection mode")
+                # print("Pen, rubber and send result selection mode")
                 if y1 < 125:
-                    if x1 > 1100 and x1 < 1280:
-                        draw_color = (0, 0, 0)
-                    if x1 > 1000 and x1 < 1100:
+                    if 1000 < x1 < 1100:
                         draw_color = (0, 0, 255)
+                    elif 1100 < x1 < 1280:
+                        draw_color = (0, 0, 0)
+                    elif 590 < x1 < 690 and (end_time - start_time) > 5:
+                        draw_finished = True
+
+
                 cv2.rectangle(frame, (x1, y1 - 25), (x2, y2 + 25), draw_color, cv2.FILLED)
 
-            print(x1)
             if fingers[1] and not fingers[2]:
-                print("entrei")
+                # print("entrei")
                 cv2.circle(frame, (x1, y1), 15, (0, 0, 255), cv2.FILLED)
-                print("Drawing mode")
+                # print("Drawing mode")
                 if xp == 0 and yp == 0:
                     xp, yp = x1, y1
 
                 if draw_color == (0, 0, 0):
-                    cv2.line(frame, (xp, yp), (x1, y1), draw_color, 125)
-                    cv2.line(img_canvas, (xp, yp), (x1, y1), draw_color, 125)
+                    cv2.line(frame, (xp, yp), (x1, y1), draw_color, 225)
+                    cv2.line(img_canvas, (xp, yp), (x1, y1), draw_color, 225)
                 else:
-                    cv2.line(frame, (xp, yp), (x1, y1), draw_color, 25)
-                    cv2.line(img_canvas, (xp, yp), (x1, y1), draw_color, 25)
+                    cv2.line(frame, (xp, yp), (x1, y1), draw_color, 45)
+                    cv2.line(img_canvas, (xp, yp), (x1, y1), draw_color, 45)
 
                 xp, yp = x1, y1
 
@@ -64,7 +93,25 @@ while True:
         frame = cv2.bitwise_or(frame, img_canvas)
 
         frame[0:125, 0:1280] = header_img
+        cv2.putText(frame, actual_label, (310, 95), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+        # print(nn_prediction)
+        cv2.putText(frame, nn_prediction, (800, 105), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
         # frame = cv2.addWeighted(frame, 8.5, img_canvas, 0.5, 0)
         cv2.imshow("Frame", frame)
-        cv2.imshow("Canvas", img_canvas)
+        img_teste = cv2.resize(img_inv, (28, 28))
+        cv2.imshow("img_inv", img_teste)
+        cv2.imshow("img_invvv", img_inv)
         cv2.waitKey(1)
+
+        if draw_finished and (end_time - start_time) > 5:
+            processed_img = process_image(img_canvas, (28, 28))
+            preds = model.predict(tf.expand_dims(processed_img, axis=0))
+            num = max(preds[0])
+            idx = list(preds[0]).index(num)
+            nn_prediction = labels[idx]
+            img_canvas = np.zeros((720, 1280, 3), np.uint8)
+            print(end_time - start_time)
+            start_time = time.time()
+            print(nn_prediction)
+
+    end_time = time.time()
